16c1fa56470717a7b883cbf4a71ba7e9ee58b420
weiwenchen <kxc0079@gmail.com>
2023-12-29 19:00:12 +0800

    Feature: basic function

---
+++ Readme.md
@@ -0,0 +1,1 @@
+run bridge.js to transmit WebSocket message to UDP
--- app.js
+++
@@ -1,9 +0,0 @@
-console.log('2');
-document.write('123');
-
-import { Client } from 'https://cdn.jsdelivr.net/npm/node-osc@9.0.2/+esm';
-
-const client = new Client('127.0.0.1', 3333);
-client.send('/oscAddress', 200, () => {
-  client.close();
-});
